f7b8701efec965b18b63561894b89b670906737c
Juli <julianalicode@gmail.com>
2023-12-28 15:14:23 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -137,4 +137,170 @@ function getTime():number{
     return value**exponent;
  }
 
- 
+ function add(a:number,b:number,...rest:number[]){
+    return a+b+rest.reduce((p,c)=>p+c,0);
+ }
+
+ //casting with as
+ let x:unknown =' hello';
+ console.log((x as string).length);
+
+ //using <> works the same as casting with as
+ let x:unknown = 'hello';
+ console.log((<string>x).length);
+
+ //Members of a class(properties&methods) are types using type annotations 
+ class Person{
+    name:string;
+ }
+ const person = new Person();
+ person.name = "ju"
+
+class Person {
+    private name:string;
+    public constructor(name:string){
+        this.name = name;
+    }
+    public getName():string{
+        return this.name;
+    }
+}
+
+const person = new Person("ju");
+
+//readonly 
+class Person {
+    private readonly name:string;
+    public constructor(name:string){
+        this.name = name;
+    }
+    public getName():string{
+        return this.name;
+    }
+}
+
+interface Shape{
+    getArea:()=>number;
+}
+class Rectangle implements Shape{
+    public constructor(protected readonly width:number,protected readonly height:number){
+
+    }
+    public getArea():number{
+        return this.width*this.height;
+    }
+}
+
+//abstract class 
+abstract class Ploygon{
+    public abstract getArea():number;
+    public toString():string{
+        return `Polygon[area=${this.getArea()}]`;
+    }
+}
+
+class Rectangle extends Ploygon{
+    public constructor(protected readonly width:number,protected readonly height:number){
+        super();
+    }
+    public getArea():number{
+        return this.width*this.height;
+    }
+}
+
+function createPair<S,T>(v1:s,v2:T):[S,T]{
+    return [v1,v2];
+}
+console.log(createPair<string,number>('hello',42));
+
+class NamedValue<T>{
+    private _value:T|undefined;
+    constructor(private name:string){
+
+    }
+    public setValue(value:T){
+        this._value =value;
+    }
+
+}
+//Partial changes all the properties in an object to be optional
+interface Point{
+    x:number;
+    y:number;
+}
+
+let pointPart:Partial<Point>={};
+pointPart.x=10;
+
+//Required changes all the properties in an object to be required
+interface Car {
+    make:string;
+    model:string;
+    mileage?:number;
+}
+let myCar:Required<Car> ={
+    make:'Ford',
+    model:'Focus',
+    mileage:12000
+};
+
+//record is shortcut to defining an object tyoe with a specific key type and value
+const nameAgeMap : Record<string,number>={
+    'Alice':21
+};
+
+// Record<string,number>  = {[key:string]:number}
+
+//Omit removes keys from an object type
+interface Person {
+    name:string;
+    age:number;
+    location?:string;
+}
+
+const bob:Omit<Person,'age'|'location'>={
+    name:'Bob'
+}
+
+//Pick removes all but the specified keys from an object Type
+interface Person {
+    name:string;
+    age:number;
+    location?:string;
+}
+
+const bob:Pick<Person,'name'>={
+    name:'Bob'
+};
+
+//Exclude remove types from a union
+type Primitive = string |number|boolean;
+const value:Exclude<Primitive,string>=true;
+
+//ReturnType extracts the return types of a function type
+type PointGenerator =()=>{x:number;y:number;};
+const point:ReturnType<PointGenerator>={
+    x:10,
+    y:20
+};
+
+//Parameters extracts the parameter types of unction type as an array
+type PointPrinter =(p:{x:number;y:number;}) =>void;
+const point:Parameters<PointPrinter>[0] ={
+    x:10,
+    y:20
+};
+
+
+interface Person{
+    name:string;
+    age:number;
+}
+const person:Readonly<Person>={
+    name:"ju",
+    age:10
+}
+
+let value:string|undefined|null=null;
+value='hello';
+value=undefined;
