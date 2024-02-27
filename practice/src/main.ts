a265f3b3cb7748cbdfbd5b9c974d877a9c9115e2
Juli <julianalicode@gmail.com>
2024-02-27 16:28:09 -0500

    mixed array

--- practice/src/main.ts
+++ practice/src/main.ts
@@ -1,8 +1,28 @@
-let username = "ju"
+let username:String = "ju"
 console.log(username);
+let album:string|number;
 
 let a:number =12
 let b:number=6
-let c :number= 2
+let c:number= 2
 
 console.log(a/b)
+
+const sum = (a:number,b:number)=>{
+    return a + b
+}
+let re:RegExp = /\w+/g
+
+let stringArr = ['one','two','three']
+let object = ['string',200]
+let mix = ['string',200,true]
+
+stringArr[0] = 'Lisa'
+stringArr.push('James')
+stringArr.unshift('Jason')
+
+let names:string[] = []
+names.push('one')
+
+//tuple
+let myTuple:[string,number,boolean] = ['one',1,true]
